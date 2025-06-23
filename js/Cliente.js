38ddc0da99fb562fbcfabddde2599c3dc3519e8d
20240153@ricaldone.edu.sv <20240153@ricaldone.edu.sv>
2025-06-23 15:45:39 -0600

    pequeños cambios

--- js/Cliente.js
+++ js/Cliente.js
@@ -1,6 +1,6 @@
 const  API_URL = "https://6859bd389f6ef961115417bf.mockapi.io/productos/productos";
 
-
+const supabaseClient = 'https://api.imgbb.com/1/upload?key=b6dd0a617dee3698d775783b9230ee1a';
 
 async function ObtenerProductos(){
     const res = await fetch(API_URL);
@@ -9,20 +9,22 @@ async function ObtenerProductos(){
     const postres = data.filter(p => p.categoria == 1);
     const bebidas = data.filter(p => p.categoria == 2);
 
+    console.log(data)
     CrearCarrusel(postres, "carouselInnerPostres");
     CrearCarrusel(bebidas, "carouselInnerBebidas");
 }
 
-const supabaseClient = 'https://api.imgbb.com/1/upload?key=b6dd0a617dee3698d775783b9230ee1a'
 
-function CrearCarrusel(productos, contenedorID){
-    const contenedor = document.getElementById(contenedorID);
+
+function CrearCarrusel(productos, contenedorId){
+    const contenedor = document.getElementById(contenedorId);
     contenedor.innerHTML= "";
+
 
     const Contenedorsize = 4;
     for(let i = 0 ; i < productos.length; i += Contenedorsize){
         const grupo = productos.slice(i, i + Contenedorsize);
-        const esActivo = 1 === 0 ? "Active" : "";
+        const esActivo = i === 0 ? "Active" : "";
 
         let slideHTML = `<div class = "carousel-item ${esActivo}><div class="row">`;
 
@@ -35,7 +37,7 @@ function CrearCarrusel(productos, contenedorID){
                   <h5 class="card-title fw-bold">${p.nombre}</h5>
                   <p class="card-text">${p.descripcion}</p>
                   <div class="d-flex justify-content-between align-items-center">
-                    <span class="fw-bold" style="color: #4d2f24;">${p.precio}</span>
+                    <span class="fw-bold" style="color: #4d2f24;">$${p.precio}</span>
                     <button class="btn px-3 py-1 rounded-pill" style="background-color: #c98e7e; color: white;">comprar</button>
                   </div>
                 </div>
@@ -44,6 +46,7 @@ function CrearCarrusel(productos, contenedorID){
         });
         slideHTML += `<div></div>`
         contenedor.innerHTML += slideHTML;
+        console.log(contenedor)
     }
 }
 
